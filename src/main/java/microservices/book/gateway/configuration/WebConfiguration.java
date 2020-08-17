b3e04c5110768a62078913bc99095cd47465d14b
Eman <eman.cse2008@gmail.com>
2020-08-17 08:19:56 +0200

    initial commit

---
+++ src/main/java/microservices/book/gateway/configuration/WebConfiguration.java
@@ -0,0 +1,18 @@
+package microservices.book.gateway.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     * More info: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
+     * @param registry
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
